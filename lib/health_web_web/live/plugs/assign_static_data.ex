479945c1213179d20badf4da3b8ec7d724385d52
chaiit2199 <chaiit2199@gmail.com>
2025-04-21 19:16:12 +0700

    fix call api

--- lib/health_web_web/live/plugs/assign_static_data.ex
+++ lib/health_web_web/live/plugs/assign_static_data.ex
@@ -1,14 +1,15 @@
 defmodule HealthWebWeb.AssignStaticData do
   use HealthWebWeb, :live_view
 
-  @api_url Application.get_env(:health_web, :base_url, [])
 
   def on_mount(:fetch_static_data, _params, _session, socket) do
     {:cont, assign(socket, static_data: fetch_diseases())}
   end
 
   defp fetch_diseases() do
-    case HTTPoison.get("#{@api_url}/diseases") do
+    base_url Application.get_env(:health_web, :base_url, [])
+
+    case HTTPoison.get("#{base_url}/diseases") do
       {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
         diseases = Jason.decode!(body)
 
