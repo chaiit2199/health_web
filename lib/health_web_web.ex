d698c9515c8ebda925c5205752b45f83d2613635
chaiit2199 <hung.tran@kafi.vn>
2025-04-28 15:54:44 +0700

    add ads.txt

--- lib/health_web_web.ex
+++ lib/health_web_web.ex
@@ -17,7 +17,7 @@ defmodule HealthWebWeb do
   those modules here.
   """
 
-  def static_paths, do: ~w(assets fonts images favicon.ico robots.txt)
+  def static_paths, do: ~w(assets fonts images favicon.ico robots.txt ads.txt)
 
   def router do
     quote do
